8e33f2933c7262f661d6f9dd4c3e887d46547d52
javier pena <123456789+odin@users.noreply.github.com>
2026-06-06 23:07:37 -0400

    task completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
+  if (num < 0) return "ERROR";
 
+  let string = "";
+  for (let i = 0; i < num; i++) {
+    string += word;
+  }
+  return string;
 };
 
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
