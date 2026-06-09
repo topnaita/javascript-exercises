7dc189802679c08f9c01f508e5e5e63bc0c59e3b
javier pena <123456789+odin@users.noreply.github.com>
2026-06-08 20:18:38 -0400

    This exercise is completed

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 === 0) {
+    return true; // Divisible by 400? Leap year.
+  } else if (year % 100 === 0) {
+    return false; // Divisible by 100 but not 400? Not a leap year.
+  } else if (year % 4 === 0) {
+    return true; // Divisible by 4 but not 100? Leap year.
+  } else {
+    return false; // Anything else is not a leap year.
+  }
 };
-
+leapYears(1985);
 // Do not edit below this line
 module.exports = leapYears;
