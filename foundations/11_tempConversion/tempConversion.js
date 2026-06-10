79c5d568dd731e77db3ad0aa8ccf9cae3084741b
javier pena <123456789+odin@users.noreply.github.com>
2026-06-09 22:42:28 -0400

    exersive completed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
-
+convertToCelsius(32);
+convertToFahrenheit(0);
+convertToCelsius(100);
+// convertToFahrenheit(100);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
