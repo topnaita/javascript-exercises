a56627c77c90bdb38b8e7a714ec75a70c2ea9ab4
javier pena <123456789+odin@users.noreply.github.com>
2026-08-01 23:02:58 -0400

    Exercises completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  arr = [1, 4, 2];
+  const sum = arr.reduce((total, current) => total + current, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  arr = [2, 4];
+  const mulArray = arr.reduce((total, current) => total * current, 1);
+  return mulArray;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
